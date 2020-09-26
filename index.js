5a0b027b676b42a53c10c226333e9ebbcad6413a
CLown1331 <clown1331@gmail.com>
2020-09-26 16:48:48 +0600

    Update

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-"use strict"
-const express = require("express");
-const port = process.env.PORT || 8080
+'use strict'
+const express = require('express');
+const logger = require('./logger');
+const port = process.env.PORT || 8080;
 
 const app = express();
-const winston = require('winston')
-const consoleTransport = new winston.transports.Console()
-const winstonOptions = {
-    transports: [consoleTransport]
-}
-const logger = new winston.createLogger(winstonOptions)
 
 app.get('/ping', (req, res) => {
     logger.info('ping-ponged');
