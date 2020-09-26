e28db7d30bb21899d5a7ffc9758f96ed9b05bcdf
CLown1331 <clown1331@gmail.com>
2020-09-26 16:42:36 +0600

    Update

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require("express");
 const port = process.env.PORT || 8080
 
 const app = express();
+const winston = require('winston')
+const consoleTransport = new winston.transports.Console()
+const winstonOptions = {
+    transports: [consoleTransport]
+}
+const logger = new winston.createLogger(winstonOptions)
 
 app.get('/ping', (req, res) => {
+    logger.info('ping-ponged');
     res.send('pong');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => logger.info(`Example app listening on port ${port}!`))
